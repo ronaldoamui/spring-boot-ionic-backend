a0fec6112255b02ceb2b0f095870b8d345466403
ronaldoamui <ronaldoamuikb@hotmail.com>
2020-10-29 10:51:30 -0300

    Correções: update em CategoriaService e contrutor em pagamento

--- src/main/java/com/ronaldo/cursomc/services/CategoriaService.java
+++ src/main/java/com/ronaldo/cursomc/services/CategoriaService.java
@@ -16,55 +16,56 @@ import com.ronaldo.cursomc.repositories.CategoriaRepository;
 import com.ronaldo.cursomc.services.exceptions.DataIntegrityException;
 import com.ronaldo.cursomc.services.exceptions.ObjectNotFoundException;
 
-
-
-
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-					
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
-		
+
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
-	public Page<Categoria> findPage(Integer page, Integer linsPerPage, String orderBy, String direction){
-		
-		PageRequest pageRequest = PageRequest.of(page, linsPerPage,Direction.valueOf(direction), orderBy);
+
+	public Page<Categoria> findPage(Integer page, Integer linsPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linsPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
-	
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+
 }
