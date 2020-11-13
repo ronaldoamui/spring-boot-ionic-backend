95921b03b9b7742da6c3b3eeeeeb649266fea759
ronaldoamui <ronaldoamuikb@hotmail.com>
2020-11-13 11:15:54 -0300

    Busca de produtos por nome e categoria

---
+++ src/main/java/com/ronaldo/cursomc/resources/utils/URL.java
@@ -0,0 +1,29 @@
+package com.ronaldo.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+
+	public static List<Integer> decodeIntList(String s) {
+		String[] vet = s.split(",");
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < vet.length; i++) {
+			list.add(Integer.parseInt(vet[i]));
+		}
+		return list;
+		// Essa linha abaixo faz a mesma coisa que todo o método acima
+		// return Arrays.asList(s.split(",")).stream().map(x ->
+		// Integer.parseInt(x)).collect(Collectors.toList());
+	}
+
+	public static String decodeParam(String s){
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
